3c351516ab38ed3a34f7e06535feee2bfa117248
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 16:30:16 +0530

    REFACTORED: Removed unwanted spaces

--- src/com/bridgelabz/logicalprograms/FibonacciSeries.java
+++ src/com/bridgelabz/logicalprograms/FibonacciSeries.java
@@ -7,7 +7,6 @@ public class FibonacciSeries {
 		int fibonacciLimit = 15;
 		int previousNumber = 0, currentNumber = 1;
 		System.out.print(previousNumber + ", " + currentNumber);
-
 		for (int index = 2; index <= fibonacciLimit; index++) {
 			int nextNumber = previousNumber + currentNumber;
 			previousNumber = currentNumber;
