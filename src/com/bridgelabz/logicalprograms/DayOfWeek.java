a03cee16cd095b86915a084e3b7f42f7c0c77280
srinadh1304 <srinadh1304@gmail.com>
2021-09-04 16:29:55 +0530

    REFACTORED: Removed unwanted spaces

--- src/com/bridgelabz/logicalprograms/DayOfWeek.java
+++ src/com/bridgelabz/logicalprograms/DayOfWeek.java
@@ -7,15 +7,14 @@ public class DayOfWeek {
 		int day = Integer.parseInt(args[0]);
 		int month = Integer.parseInt(args[1]);
 		int year = Integer.parseInt(args[2]);
-
 		dayOfWeek(month,day,year);
 	}
-	private static void dayOfWeek(int m, int d, int y) {
-		int y0 = y - (14-m) / 12;
+	private static void dayOfWeek(int month, int day, int year) {
+		int y0 = year - (14-month) / 12;
 		int x = y0 + (y0/4)- (y0/100) + (y0/400);
-		int m0 = m + 12 * ((14-m)/12)-2;
-		int d0 = (d+x+31*m0/12)% 7;
-		System.out.println("The entered date is : "+ d+"/"+m+"/"+y);
+		int m0 = month + 12 * ((14-month)/12)-2;
+		int d0 = (day+x+31*m0/12)% 7;
+		System.out.println("The entered date is : "+ day+"/"+month+"/"+year);
 		System.out.println("The day is: "+weeks[d0]);
 	}
 
